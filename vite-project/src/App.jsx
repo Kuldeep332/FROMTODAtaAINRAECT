a0e4dee804e53faa5bd59ef167a8b197a436cb2c
kuldeep <kuldeepdhakad152@gmail.com>
2024-05-01 18:29:10 +0530

    apiadd

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -4,6 +4,14 @@ function App() {
 
   const [username, setuser] = useState("")
   const [email, setuser1] = useState("")
+  const [gender, setgender] = useState("male")
+  const [city, setcity] = useState("delhi")
+  const [desc, setdes] = useState("")
+  const [acc, setacc] = useState("true")
+
+
+
+
 
   const handeler=(event)=>{
     event.preventDefault();
@@ -12,7 +20,7 @@ function App() {
 
 // console.log(event.target.username.value);
 // console.log(event.target.email.value);
-console.log({username})
+console.log({username},city,gender,desc)
 console.log({email})
 
 
@@ -26,31 +34,43 @@ console.log({email})
     <input onChange={(e)=>setuser(e.target.value)} name='username' value={username} type="text" placeholder='username' />
     <br></br>
 
-    <input onChange={(e)=>setuser1(e.target.value)} name='email' value={email} type="text" placeholder='email' />
+    <input onChange={(e)=>setuser1(e.target.value)} name='email'
+    checked={gender=="male"?true:false}
+    value={email} type="text" placeholder='email' />
     <br></br>
-    <input type="radio"  name='gender'/>
+    <input onChange={(e)=>(e.target.value)} 
+    value="male"
+        checked={gender==="male"?true:false}
+
+     type="radio"  name='gender' />
     male
     <br></br>
 
-    <input type="radio"  name='gender'/>
+    <input onChange={(e)=>{e.target.value}} type="radio"      value="female"
+ name='gender'/>
     female
     <br></br>
 
-    <select>
-      <option>Delhi</option>
-      <option>pune</option>
-      <option>mp</option>
-      <option>up</option>
+    <select 
+    defaultValue={city}
+    onChange={(e)=>setcity(e.target.value)}
+    >
+      <option value="delhi">Delhi</option>
+      <option value="pune">pune</option>
+      <option  value="mp">mp</option>
+      <option  value="up">up</option>
 
     </select>
     <br></br>
 
-<textarea name="" id="" cols="20" rows="3" placeholder='enter your text'>
+<textarea onChange={(e)=>setdes(e.target.value)} value={desc} name="" id="" cols="20" rows="3" placeholder='enter your text'>
 
 </textarea>
 <br></br>
 
-<input type="checkbox" />
+<input 
+checked={acc?true:false}
+onChange={(e)=>setacc(e.target.checked)} type="checkbox" />
 <br></br>
 
     <button>submit</button>
